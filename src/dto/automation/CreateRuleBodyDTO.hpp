4af0f046fe319aac44e0f4369d80a4e1325c522a
Oussama DAHMAZ <oussama.dahmaz@gmail.com>
2021-01-04 20:06:20 +0100

    move InterpreterType & EditorType to specific files to be used bu others

--- src/dto/automation/CreateRuleBodyDTO.hpp
+++ src/dto/automation/CreateRuleBodyDTO.hpp
@@ -3,25 +3,17 @@
 
 #include "oatpp/core/macro/codegen.hpp"
 #include "oatpp/core/Types.hpp"
+#include "dto/automation/InterpreterTypeDTO.hpp"
+#include "dto/automation/EditorTypeDTO.hpp"
 
 #include OATPP_CODEGEN_BEGIN(DTO)
-
-ENUM(InterpreterType, v_int32,
-   VALUE(YPYTHON3, 1, "yPython3"),
-   VALUE(YPYTHON2, 2, "yPython2")
-);
-
-ENUM(EditorType, v_int32,
-   VALUE(CODE, 1, "code"),
-   VALUE(BLOCKLY, 2, "blockly")
-);
 
 class CreateRuleBody : public oatpp::DTO
 {
    DTO_INIT(CreateRuleBody, DTO);
    DTO_FIELD(String, name);
    DTO_FIELD(String, description);
-   DTO_FIELD(Enum<InterpreterType>::AsString, interpreter);
+   DTO_FIELD(Enum<InterpreterType>, interpreter);
    DTO_FIELD(Enum<EditorType>, editor);
    DTO_FIELD(String, model);
    DTO_FIELD(String, content);
---
+++ src/dto/automation/EditorTypeDTO.hpp
@@ -0,0 +1,16 @@
+﻿#ifndef EditorTypeDTO_hpp
+#define EditorTypeDTO_hpp
+
+#include "oatpp/core/macro/codegen.hpp"
+#include "oatpp/core/Types.hpp"
+
+#include OATPP_CODEGEN_BEGIN(DTO)
+
+ENUM(EditorType, v_int32,
+   VALUE(CODE, 1, "code"),
+   VALUE(BLOCKLY, 2, "blockly")
+);
+
+#include OATPP_CODEGEN_END(DTO)
+
+#endif /* EditorTypeDTO_hpp */
