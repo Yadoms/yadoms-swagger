76354317408e81c379edd3732fdc1c42a13212be
Oussama DAHMAZ <oussama.dahmaz@gmail.com>
2021-01-10 15:38:48 +0100

    #6 add getOnePage

--- src/controller/PageController.hpp
+++ src/controller/PageController.hpp
@@ -4,6 +4,7 @@
 #include "oatpp/core/macro/codegen.hpp"
 #include "oatpp/core/macro/component.hpp"
 #include "dto/page/PagesResponseDTO.hpp"
+#include "dto/page/PageResponseDTO.hpp"
 #include OATPP_CODEGEN_BEGIN(ApiController)
 
 class CPageController : public oatpp::web::server::api::ApiController
@@ -24,6 +25,21 @@ public:
          );
    }
    ENDPOINT("GET", "/page", getAllPages) {
+      return createResponse(Status::CODE_200, "OK");
+   }
+
+   ENDPOINT_INFO(getOnePage)
+   {
+      info->addTag("Page");
+      info->summary = "Get one page";
+      info->description = "Get page informations";
+      info->addResponse<Object<PageResponse>>(
+         Status::CODE_200,
+         "application/json"
+         );
+   }
+   ENDPOINT("GET", "/page/{pageId}", getOnePage, 
+            PATH(Int32, pageId)) {
       return createResponse(Status::CODE_200, "OK");
    }
 };
---
+++ src/dto/page/PageResponseDTO.hpp
@@ -0,0 +1,21 @@
+﻿#ifndef PageRESPONSEDTO_hpp
+#define PageRESPONSEDTO_hpp
+
+#include "oatpp/core/macro/codegen.hpp"
+#include "oatpp/core/Types.hpp"
+#include "dto/shared/CommonsDataDTO.hpp"
+#include "dto/page/PageDataDTO.hpp"
+#include OATPP_CODEGEN_BEGIN(DTO)
+
+class PageResponse : public CommonsData
+{
+   DTO_INIT(PageResponse, CommonsData);
+   DTO_FIELD(Object<PageData>, data);
+};
+
+#include OATPP_CODEGEN_END(DTO)
+
+#endif /* PageRESPONSEDTO_hpp */
+
+
+
