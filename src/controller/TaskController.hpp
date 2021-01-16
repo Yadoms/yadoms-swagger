fdc7dd05a586488ce9fa08918a42a921adc14887
Oussama DAHMAZ <oussama.dahmaz@gmail.com>
2021-01-16 19:57:17 +0100

    #11 add getOneTask

--- src/controller/TaskController.hpp
+++ src/controller/TaskController.hpp
@@ -4,6 +4,7 @@
 #include "oatpp/core/macro/codegen.hpp"
 #include "oatpp/core/macro/component.hpp"
 #include "dto/task/TasksResponseDTO.hpp"
+#include "dto/task/TaskResponseDTO.hpp"
 #include OATPP_CODEGEN_BEGIN(ApiController)
 
 class CTaskController : public oatpp::web::server::api::ApiController
@@ -18,12 +19,28 @@ public:
       info->addTag("Task");
       info->summary = "Get all tasks";
       info->description = "List all tasks";
-      info->addResponse<Object<TaskResponse>>(
+      info->addResponse<Object<TasksResponse>>(
          Status::CODE_200,
          "application/json"
          );
    }
    ENDPOINT("GET", "/task", getAllTasks) {
+      return createResponse(Status::CODE_200, "OK");
+   }
+
+   ENDPOINT_INFO(getOneTask)
+   {
+      info->addTag("Task");
+      info->summary = "Get one task";
+      info->description = "Get one task";
+      info->addResponse<Object<TaskResponse>>(
+         Status::CODE_200,
+         "application/json"
+         );
+   }
+   ENDPOINT("GET", "/task/{taskUuid}", getOneTask,
+            PATH(String, taskUuid))
+   {
       return createResponse(Status::CODE_200, "OK");
    }
 };
--- src/dto/task/TaskResponseDTO.hpp
+++ src/dto/task/TaskResponseDTO.hpp
@@ -1,5 +1,5 @@
-﻿#ifndef TasksRESPONSEDTO_hpp
-#define TasksRESPONSEDTO_hpp
+﻿#ifndef TaskRESPONSEDTO_hpp
+#define TaskRESPONSEDTO_hpp
 
 #include "oatpp/core/macro/codegen.hpp"
 #include "oatpp/core/Types.hpp"
@@ -7,10 +7,10 @@
 #include "dto/task/TaskDTO.hpp"
 #include OATPP_CODEGEN_BEGIN(DTO)
 
-class TasksResponse : public CommonsData
+class TaskResponse : public CommonsData
 {
-   DTO_INIT(TasksResponse, CommonsData);
-   DTO_FIELD(Fields<Vector<Object<Task>>>, data) = { {"task", Vector<Object<Task>>({Task::createShared()})} };
+   DTO_INIT(TaskResponse, CommonsData);
+   DTO_FIELD(Object<Task> , data);
 };
 
 #include OATPP_CODEGEN_END(DTO)
